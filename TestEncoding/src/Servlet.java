e3978c96bdb894b150f507af0a1835782ca05c99
TMarinov <TMarinov@marinov.hissar-pdc.com>
2016-11-22 09:13:36 +0200

    Initial Configuration

---
+++ TestEncoding/src/Servlet.java
@@ -0,0 +1,34 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public Servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		System.out.println("Сървлета работи. Аз се казвам Дончо.");
+		String name = request.getParameter("name");
+		System.out.println("Сървлета работи. Аз се казвам " + name + "!");
+	}
+
+}
